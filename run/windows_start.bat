ed6a3e3e3f64dfb49b7739ebf6dde9f6f0394af7
Mehtaab Gill <37054853+mehtaabGill@users.noreply.github.com>
2019-01-24 15:35:19 -0700

    Update windows_start.bat

--- run/windows_start.bat
+++ run/windows_start.bat
@@ -3,10 +3,6 @@
 echo starting instapy with quickstart
 echo ================================
 
-REM[find the current directory and removes "/run" from the end to get the instapy-quickstart directory"]
-set CD=%CD:~0,-4%
-
-cd %CD%
-python quickstart.py
+python ../quickstart.py
 
 pause
