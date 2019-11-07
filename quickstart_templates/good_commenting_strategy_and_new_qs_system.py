a21f3ec8a271e22abfbd9048368e8f8280118943
Anirban Dey <anirbandey303@gmail.com>
2019-11-07 14:19:15 +0530

    Updated with new parameter names for good_commentating_strategy_and_new_qs_system.py

--- quickstart_templates/good_commenting_strategy_and_new_qs_system.py
+++ quickstart_templates/good_commenting_strategy_and_new_qs_system.py
@@ -25,8 +25,7 @@ from instapy import InstaPy
 from instapy import smart_run
 
 # get a session!
-session = InstaPy(username='xxx', password='xxx', proxy_address='xxx',
-                  proxy_port='xxxx', headless_browser=True)
+session = InstaPy(username='xxxx', password='xxxx', headless_browser=True)
 
 # let's go! :>
 with smart_run(session):
@@ -53,8 +52,8 @@ with smart_run(session):
 
     # general settings
     session.set_dont_like(['sad', 'rain', 'depression'])
-    session.set_do_follow(enabled=True, percentage=40, times=1)
-    session.set_do_comment(enabled=True, percentage=30)
+    session.set_do_follow(enabled=True, percentage=80, times=1)
+    session.set_do_comment(enabled=True, percentage=80)
     session.set_comments([
                              u'What an amazing shot! :heart_eyes: What do '
                              u'you think of my recent shot?',
@@ -79,37 +78,40 @@ with smart_run(session):
                              u'Great capture!! :smiley: :thumbsup: What do '
                              u'you think of my recent photo?'],
                          media='Photo')
-    session.set_do_like(True, percentage=70)
-    session.set_delimit_liking(enabled=True, max_likes=100, min_likes=0)
-    session.set_delimit_commenting(enabled=True, max_comments=20, min_comments=0)
+    session.set_do_like(True, percentage=80)
+    session.set_delimit_liking(enabled=True, max_likes=200, min_likes=0)
+    session.set_delimit_commenting(enabled=True, max_comments=80, min_comments=0)
     session.set_relationship_bounds(enabled=True,
                                     potency_ratio=None,
                                     delimit_by_numbers=True,
                                     max_followers=3000,
                                     max_following=2000,
-                                    min_followers=100,
+                                    min_followers=50,
                                     min_following=50)
 
     session.set_quota_supervisor(enabled=True,
                                  sleep_after=["likes", "follows"],
                                  sleepyhead=True, stochastic_flow=True,
                                  notify_me=True,
-                                 peak_likes_hourly=100,
-                                 peak_likes_daily=1000,
-                                 peak_comments_daily=250,
-                                 peak_follows_hourly=200,
-                                 peak_follows_daily=None)
+                                 peak_likes_hourly=200,
+                                 peak_likes_daily=585,
+                                 peak_comments_hourly=80,
+                                 peak_comments_daily=182,
+                                 peak_follows_hourly=48,
+                                 peak_follows_daily=None,
+                                 peak_unfollows_hourly=35,
+                                 peak_unfollows_daily=402,
+                                 peak_server_calls_hourly=None,
+                                 peak_server_calls_daily=4700)
 
-    session.set_user_interact(amount=1, randomize=False, percentage=40)
+    session.set_user_interact(amount=10, randomize=True, percentage=80)
 
     # activity
-    session.like_by_tags(my_hashtags, amount=60, media=None)
-    session.unfollow_users(amount=500, instapy_followed_enabled=True,
-                            instapy_followed_param="nonfollowers",
+    session.like_by_tags(my_hashtags, amount=90, media=None)
+    session.unfollow_users(amount=500, instapy_followed_enabled=True, instapy_followed_param="nonfollowers",
                            style="FIFO",
                            unfollow_after=12 * 60 * 60, sleep_delay=501)
-    session.unfollow_users(amount=500, instapy_followed_enabled=True,
-                            instapy_followed_param="all",
+    session.unfollow_users(amount=500, instapy_followed_enabled=True, instapy_followed_param="all",
                            style="FIFO", unfollow_after=24 * 60 * 60,
                            sleep_delay=501)
 
